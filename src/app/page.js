fcc5ec3b27f6cbd906d841282c8765f68c97b1a7
krisztina nemeth <nkrisztina82@gmail.com>
2024-08-16 15:04:13 +0100

    show visitor number hits on page

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,35 @@
 import React from 'react';
+import { readFile, writeFile } from '../helpers/fileHelpers';
+
+const DATABASE_PATH = '/src/database.json';
+
+/*
+`readFile` takes 1 argument:
+• the path to the file:
+
+readFile('/path/to/file');
+
+`writeFile` takes 2 arguments:
+• The path to the file
+• The new contents for the file
+
+writeFile(
+  '/path/to/file',
+  '{ "hello": "world" }'
+);
+*/
 
 function Home() {
+  let { hits } = JSON.parse(readFile(DATABASE_PATH));
+
+  hits += 1;
+
+  writeFile(DATABASE_PATH, JSON.stringify({ hits }));
+
   return (
     <main>
       <h1>Hello Next!</h1>
+      <p>You are visitor number: {hits}</p>
     </main>
   );
 }
