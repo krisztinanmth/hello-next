475b0404e6dd80c159750050d2cfc7561b5877b5
krisztina nemeth <nkrisztina82@gmail.com>
2024-08-16 14:49:16 +0100

    footer - add timestamp when HTML is rendered on server

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,11 +3,16 @@ import React from 'react';
 import './styles.css';
 
 function RootLayout({ children }) {
+  const timestamp = new Date().toLocaleString();
+
   return (
     <html lang="en">
       <body>
         {children}
-
+        <footer>
+          page render at:
+          {timestamp}
+        </footer>
       </body>
     </html>
   );
